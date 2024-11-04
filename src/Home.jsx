a554e1904847bfe1cd1395b76d2d0d9116c0b606
t3ugeg00 <t3ugeg00@students.oamk.fi>
2024-11-04 19:36:25 +0200

    Added body

--- src/Home.jsx
+++ src/Home.jsx
@@ -32,14 +32,89 @@ export default function Home() {
                         />
                     </Col>
                     <Col xs="auto">
-                        <Button type="submit" variant="outline-success">Submit</Button>
+                        <Button type="submit" variant="outline-success">Search</Button>
                     </Col>
                     </Row>
                 </Form>
-                <Button variant="outline-dark" className="ms-0 ms-lg-5 mt-2 mt-lg-0">Log in</Button>
+                <Button as={Link} variant="outline-dark" className="ms-0 ms-lg-5 mt-2 mt-lg-0">Log in</Button>
                 </Navbar.Collapse>
             </Container>
             </Navbar>
+
+
+            <Container className="border border-1 border-dark rounded mt-5">
+                <Row className="fs-4">
+                    <Col xs={6} className="text-start fw-bold">
+                        Category 1
+                    </Col>
+                    <Col xs={6} className="text-end text-decoration-none text-dark" as={Link}>
+                        {'View all ->'}
+                    </Col>
+                </Row>
+                <Row>
+                    <Col lg={2} md={3} xs={4} className="text-center p-2 text-decoration-none text-dark" as={Link}>
+                        <img src="https://image.tmdb.org/t/p/w500/a26cQPRhJPX6GbWfQbvZdrrp9j9.jpg" className="img-fluid p-1"/>
+                        Fight Club
+                    </Col>
+                    <Col lg={2} md={3} xs={4} className="text-center p-2 text-decoration-none text-dark" as={Link}>
+                        <img src="https://image.tmdb.org/t/p/w500/a26cQPRhJPX6GbWfQbvZdrrp9j9.jpg" className="img-fluid p-1"/>
+                        Fight Club
+                    </Col>
+                    <Col lg={2} md={3} xs={4} className="text-center p-2 text-decoration-none text-dark" as={Link}>
+                        <img src="https://image.tmdb.org/t/p/w500/a26cQPRhJPX6GbWfQbvZdrrp9j9.jpg" className="img-fluid p-1"/>
+                        Fight Club
+                    </Col>
+                    <Col lg={2} xs={0} md={3} className="text-center p-2 d-none d-md-block text-decoration-none text-dark" as={Link}>
+                        <img src="https://image.tmdb.org/t/p/w500/a26cQPRhJPX6GbWfQbvZdrrp9j9.jpg" className="img-fluid p-1"/>
+                        Fight Club
+                    </Col>
+                    <Col lg={2} xs={0} className="text-center p-2 d-none d-lg-block text-decoration-none text-dark" as={Link}>
+                        <img src="https://image.tmdb.org/t/p/w500/a26cQPRhJPX6GbWfQbvZdrrp9j9.jpg" className="img-fluid p-1"/>
+                        Fight Club
+                    </Col>
+                    <Col lg={2} xs={0} className="text-center p-2 d-none d-lg-block text-decoration-none text-dark" as={Link}>
+                        <img src="https://image.tmdb.org/t/p/w500/a26cQPRhJPX6GbWfQbvZdrrp9j9.jpg" className="img-fluid p-1"/>
+                        Fight Club
+                    </Col>
+                </Row>
+            </Container>
+
+            <Container className="border border-1 border-dark rounded mt-5">
+                <Row className="fs-4">
+                    <Col xs={6} className="text-start fw-bold">
+                        Category 2
+                    </Col>
+                    <Col xs={6} className="text-end text-decoration-none text-dark" as={Link}>
+                        {'View all ->'}
+                    </Col>
+                </Row>
+                <Row>
+                    <Col lg={2} md={3} xs={4} className="text-center p-2 text-decoration-none text-dark" as={Link}>
+                        <img src="https://image.tmdb.org/t/p/original/gajva2L0rPYkEWjzgFlBXCAVBE5.jpg" className="img-fluid p-1"/>
+                        Blade Runner 2049
+                    </Col>
+                    <Col lg={2} md={3} xs={4} className="text-center p-2 text-decoration-none text-dark" as={Link}>
+                        <img src="https://image.tmdb.org/t/p/original/gajva2L0rPYkEWjzgFlBXCAVBE5.jpg" className="img-fluid p-1"/>
+                        Blade Runner 2049
+                    </Col>
+                    <Col lg={2} md={3} xs={4} className="text-center p-2 text-decoration-none text-dark" as={Link}>
+                        <img src="https://image.tmdb.org/t/p/original/gajva2L0rPYkEWjzgFlBXCAVBE5.jpg" className="img-fluid p-1"/>
+                        Blade Runner 2049
+                    </Col>
+                    <Col lg={2} xs={0} md={3} className="text-center p-2 d-none d-md-block text-decoration-none text-dark" as={Link}>
+                        <img src="https://image.tmdb.org/t/p/original/gajva2L0rPYkEWjzgFlBXCAVBE5.jpg" className="img-fluid p-1"/>
+                        Blade Runner 2049
+                    </Col>
+                    <Col lg={2} xs={0} className="text-center p-2 d-none d-lg-block text-decoration-none text-dark" as={Link}>
+                        <img src="https://image.tmdb.org/t/p/original/gajva2L0rPYkEWjzgFlBXCAVBE5.jpg" className="img-fluid p-1"/>
+                        Blade Runner 2049
+                    </Col>
+                    <Col lg={2} xs={0} className="text-center p-2 d-none d-lg-block text-decoration-none text-dark" as={Link}>
+                        <img src="https://image.tmdb.org/t/p/original/gajva2L0rPYkEWjzgFlBXCAVBE5.jpg" className="img-fluid p-1"/>
+                        Blade Runner 2049
+                    </Col>
+                </Row>
+            </Container>
         </div>
     )
 }
