311c841cc99e45592c1cc7b793d9226713cf8e6a
Ahn Sungmin <asungmin24@students.oamk.fi>
2024-11-06 20:47:38 +0200

    last one was mistake on Home.jsx sorry.

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,7 +2,6 @@ import PopularMovies from "./HomeComponents/PopularMovies.jsx";
 import TrendingMovies from "./HomeComponents/TrendingMovies.jsx";
 import PopularTVshows from "./HomeComponents/PopularTVshows.jsx";
 import Navigation from "./components/Navigation.jsx";
-import Navigation from "./components/Navigation.jsx";
 
 export default function Home() {
     return (
